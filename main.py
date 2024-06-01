dc10da10a1e176b7d24e017a1616d3f1f57a73cb
MontyMerlin <romerlin@live.co.uk>
2024-06-01 16:23:05 +0100

    flat pages

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from flask import Flask, render_template, url_for
+from flask_flatpages import FlatPages
 
 app = Flask(__name__)
+app.config['FLATPAGES_EXTENSION'] = '.md'
+app.config['FLATPAGES_ROOT'] = 'content'
+flatpages = FlatPages(app)
 
 
 @app.route('/')
@@ -10,7 +14,8 @@ def home_page():
 
 @app.route('/about')
 def about_page():
-    return render_template("about.html")
+    about_articles = flatpages.get_or_404('about')
+    return render_template("about.html", articles=about_articles)
 
 
 @app.route('/cv')
@@ -21,6 +26,11 @@ def cv_page():
 @app.route('/projects')
 def projects_page():
     return render_template("projects.html")
+
+@app.route('/test')
+def test_page():
+    page = flatpages.get_or_404('test')
+    return render_template("page.html", page=page)
 
 
 app.run(host='0.0.0.0', port=81)
